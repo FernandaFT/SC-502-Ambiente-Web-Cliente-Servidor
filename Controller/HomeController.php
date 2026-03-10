5fee16dd1d41b387c7b48ba87480001870f12fad
Fernanda Fajardo <nanda199784@gmail.com>
2026-03-09 18:12:13 -0600

    Recuperar Acceso

--- Controller/HomeController.php
+++ Controller/HomeController.php
@@ -1,4 +1,5 @@
 <?php
+include_once $_SERVER["DOCUMENT_ROOT"]."/SC-502-Ambiente-Web-Cliente-Servidor/Controller/UtilitarioController.php";
 include_once $_SERVER["DOCUMENT_ROOT"]."/SC-502-Ambiente-Web-Cliente-Servidor/Model/HomeModel.php";
 
 if(session_status() === PHP_SESSION_NONE)
@@ -40,4 +41,35 @@ if(isset($_POST["btnIniciarSesion"])){
         } else {
             $_POST["Mensaje"] = "Su información no fue autenticada correctamente.";
         }
+}
+
+if (isset($_POST["btnRecuperarAcceso"])) {
+
+    $correoElectronico = $_POST["CorreoElectronico"];
+
+    $result = ValidarCorreoModel($correoElectronico);
+
+    if ($result) {
+
+        //Generar nueva contraseña
+        $nuevaContrasenna = GenerarContrasenna();
+        $actualizacion = ActualizarContrasennaModel($nuevaContrasenna, $result["Consecutivo"]);
+
+        if ($actualizacion) {
+
+            $plantilla = file_get_contents($_SERVER["DOCUMENT_ROOT"] . "/SC-502-Ambiente-Web-Cliente-Servidor/View/emails/recuperarAcceso.html");
+            $cuerpoCorreo = str_replace(
+                ["{{NOMBRE}}", "{{CONTRASENNA}}"],
+                [$result["Nombre"], $nuevaContrasenna],
+                $plantilla
+            );
+
+            EnviarCorreo("Recuperar Acceso", $cuerpoCorreo, $result["CorreoElectronico"]);
+
+            header("Location: ../../View/vHome/login.php");
+            exit;
+        }
+    }
+
+    $_POST["Mensaje"] = "Su información no fue validada correctamente";
 }
--- View/vHome/registro.php
+++ View/vHome/registro.php
@@ -1,6 +1,6 @@
 <?php
-include_once $_SERVER["DOCUMENT_ROOT"]."/SC-502-Ambiente-Web-Cliente-Servidor/View/layoutExterno.php";
-include_once $_SERVER["DOCUMENT_ROOT"]."/SC-502-Ambiente-Web-Cliente-Servidor/Controller/HomeController.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/SC-502-Ambiente-Web-Cliente-Servidor/View/layoutExterno.php";
+include_once $_SERVER["DOCUMENT_ROOT"] . "/SC-502-Ambiente-Web-Cliente-Servidor/Controller/HomeController.php";
 ?>
 <!DOCTYPE html>
 <html lang="en">
@@ -48,49 +48,54 @@ MostrarCSS();
                                     </div>
                                 </div>
                                 <div class="col-lg-6">
-                                    <?php
-                                        if(isset($_POST["Mensaje"])){
-                                            echo $_POST["Mensaje"];
-                                        }
-                                    ?>
-                                    <div class="signin-wrapper">
+                                    <div class="signup-wrapper">
                                         <div class="form-wrapper">
-                                            <h3 class="mb-15">Registrarse</h3>
 
-                                            <form action="" method="POST" id="formRegistro">
+                                            <?php
+                                            if (isset($_POST["Mensaje"])) {
+                                                echo
+                                                '<div class="alert alert-danger text-center" role="alert">
+                                                            ' . $_POST["Mensaje"] . '
+                                                        </div>';
+                                            }
+                                            ?>
+
+                                            <h3 class="mb-15">Crear Cuenta</h3>
+
+                                            <form id="formRegistro" action="" method="POST">
                                                 <div class="row">
                                                     <div class="col-12">
                                                         <div class="input-style-1">
                                                             <label>Identificación</label>
                                                             <input type="text" placeholder="Identificación"
-                                                            id="Identificacion" name="Identificacion"/>
+                                                                id="Identificacion" name="Identificacion" onkeyup="ConsultarNombre();" />
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="input-style-1">
                                                             <label>Nombre</label>
                                                             <input type="text" placeholder="Nombre"
-                                                            id="Nombre" name="Nombre"/>
+                                                                id="Nombre" name="Nombre" class="ReadOnly" readOnly="true" />
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="input-style-1">
                                                             <label>Correo Electrónico</label>
                                                             <input type="text" placeholder="Correo Electrónico"
-                                                            id="CorreoElectronico" name="CorreoElectronico"/>
+                                                                id="CorreoElectronico" name="CorreoElectronico" />
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="input-style-1">
                                                             <label>Contraseña</label>
                                                             <input type="password" placeholder="Contraseña"
-                                                            id="Contrasenna" name="Contrasenna"/>
+                                                                id="Contrasenna" name="Contrasenna" />
                                                         </div>
                                                     </div>
                                                     <div class="col-12">
                                                         <div class="button-group d-flex justify-content-center flex-wrap">
                                                             <button type="submit" class="main-btn primary-btn btn-hover w-100 text-center"
-                                                            id="btnRegistrar" name="btnRegistrar">
+                                                                id="btnRegistrar" name="btnRegistrar">
                                                                 Procesar
                                                             </button>
                                                         </div>
@@ -98,10 +103,9 @@ MostrarCSS();
                                                 </div>
                                             </form>
 
-                                            <div class="singin-option pt-40">
+                                            <div class="singup-option pt-40">
                                                 <p class="text-sm text-medium text-dark text-center">
-                                                    Ya tiene una cuenta?
-                                                    <a href="login.php">Inicia Sesión</a>
+                                                    Ya tiene una cuenta? <a href="login.php">Inicia Sesión</a>
                                                 </p>
                                             </div>
                                         </div>
